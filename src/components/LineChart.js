233546c423105d2ef34b4a31c32f7cca2d0fd0db
jd.macmurtrie <jamesd.macmurtrie@gmail.com>
2018-09-16 13:48:48 -0400

    Finesse axises

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -23,9 +23,9 @@ class LineChart extends Component {
   createLineChart() {
     const { numbers, time } = this.props
     const node = this.node
-    const margin = { left:100, right:25, top:20, bottom:100 };
-    const width = 800 - margin.left - margin.right
-    const height = 800 - margin.top - margin.bottom
+    const margin = { left:200, right:25, top:20, bottom:200 };
+    const width = 1000 - margin.left - margin.right
+    const height = 1000 - margin.top - margin.bottom
     const getTimes = () => {
       return numbers.map((num, i) => {
         const now = new Date(time)
@@ -44,35 +44,14 @@ class LineChart extends Component {
     }
 
   const data = getTimes()
-  console.log('data', data);
   d3.selectAll('g').remove()
 
   const g = d3.select(node)
     .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
     .append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-  // // X Label
-  g.append("text")
-      .attr("class", "x axis-label")
-      .attr("x", width / 2)
-      .attr("y", height + 55)
-      .attr("font-size", "20px")
-      .attr("text-anchor", "middle")
-      .text("Time");
-
-  // Y Label
-  g.append("text")
-      .attr("class", "y axis-label")
-      .attr("x", - (height / 2))
-      .attr("y", -40)
-      .attr("font-size", "20px")
-      .attr("text-anchor", "middle")
-      .attr("transform", "rotate(-90)")
-      .text("Random Numbers");
-
+      .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
   const x = d3.scaleTime()
     .domain([
@@ -83,54 +62,62 @@ class LineChart extends Component {
 
   const getMinDomain = () => {
     const lowestNumber = d3.min(data, (d) => d.number)
-    console.log('lowest', lowestNumber);
     return lowestNumber > 0 ? 0 : lowestNumber
   }
-  console.log('domain', getMinDomain())
-  console.log('domain', d3.max(data, (d) => d.number));
 
   const y = d3.scaleLinear()
   .domain([getMinDomain(), d3.max(data, (d) => d.number)])
   .range([height, 0]);
 
+  // X-axis
+  const xAxisCall = d3.axisBottom(x)
+    // .ticks(data.length)
+    .tickFormat(d => d.toLocaleString())
+
+  g.append("g")
+    .attr("class", "x axis")
+    .attr("transform", `translate(0, ${height})`)
+    .call(xAxisCall)
+  .selectAll("text")
+    .attr("y", "10")
+    .attr("x", "-5")
+    .attr("text-anchor", "end")
+    .attr("transform", "rotate(-40)")
+    .attr("font-size", "18px")
+
+  // X Label
+  g.append("text")
+      .attr("class", "x axis-label")
+      .attr("x", width / 2)
+      .attr("y", height + 200)
+      .attr("font-size", "40px")
+      .attr("text-anchor", "middle")
+      .text("Time");
+
+
+  // Y-axis
+  g.append("g")
+    .call(d3.axisLeft(y))
+  .selectAll("text")
+    .attr("font-size", "18px")
+
+
+  // Y Label
+  g.append("text")
+      .attr("class", "y axis-label")
+      .attr("x", - (height / 2))
+      .attr("y", -60)
+      .attr("font-size", "40px")
+      .attr("text-anchor", "middle")
+      .attr("transform", "rotate(-90)")
+      .text("Random Numbers");
+
+  // Lines
   const line = d3.line()
     .x(d => x(d.time))
     .y(d => y(d.number))
   x.domain(d3.extent(data, (d) => d.time));
   y.domain(d3.extent(data, (d) => d.number));
-
-  const xAxisCall = d3.axisBottom(x)
-    .ticks(data.length)
-    .tickFormat((d) => {
-      d.toLocaleString()
-    })
-
-  g.append("g")
-    .attr("class", "x axis")
-    .attr("transform", `translate(0, ${height})`)
-    .call(d3.axisBottom(x))
-  // .select(".domain")
-
-  // g.append("g")
-  //     .attr("transform", "translate(0, " + height + ")")
-  //     .call(xAxisCall)
-  // .selectAll("text")
-  //     .attr("y", "10")
-  //     .attr("text-anchor", "center")
-
-  // const yAxisCall = d3.axisLeft(y)
-  //
-  // g.append("g")
-  //     .call(yAxisCall);
-  g.append("g")
-    .call(d3.axisLeft(y))
-    .append("text")
-      .attr("class", "y-axis")
-      .attr("fill", "#000")
-      .attr("transform", "rotate(-90)")
-      // .attr("y", 6)
-      // .attr("dy", "0.71em")
-      .attr("text-anchor", "center")
 
   g.append("path")
     .datum(data)
@@ -138,18 +125,33 @@ class LineChart extends Component {
     .attr("stroke", "steelblue")
     .attr("stroke-linejoin", "round")
     .attr("stroke-linecap", "round")
-    .attr("stroke-width", 1.5)
+    .attr("stroke-width", 3)
     .attr("d", line);
 
+  // Dots
   const circles = g.selectAll('circle')
     .data(data);
 
   circles.enter()
     .append('circle')
-    .attr("cx", (d) => (x(d.time)))
-    .attr("cy", (d) => (y(d.number)))
-    .attr("r", 4)
+    .attr("cx", (d) => x(d.time))
+    .attr("cy", (d) => y(d.number))
+    .attr("r", 6)
     .attr("fill", "steelblue")
+
+
+  const labels = g.selectAll('text')
+    .data(data)
+
+  labels.enter()
+    .append('text')
+    .text(d => d.number)
+    .attr("x", (d) => {
+      return x(d.time);
+    })
+    .attr("y", (d) => {
+      return y(d.number);
+    });
 }
 
   render() {
